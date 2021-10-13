ea1af2c06a9e83dc8e5c2f54e97e3ef98dceacb0
Sergei-k23 <61871599+Sergei-k23@users.noreply.github.com>
2021-10-13 19:16:21 +0300

    Update main.tf

--- labs/variables-and-tfvars/main.tf
+++ labs/variables-and-tfvars/main.tf
@@ -1,22 +1,3 @@
-#----variables-----
-
-variable "container_name" {
-  description = "Name of container"
-  default = "blog"
-}
-variable "image" {
-  description = "image for container"
-  default = "ghost:latest"
-}
-variable "int_port" {
-  description = "internal port for container"
-  default = "2368"
-}
-variable "ext_port" {
-  description = "external port for container"
-  default = "80"
-}
-
 #------resources--------
 
 # Download the latest Ghost Image
